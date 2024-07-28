fba1160065fa4dc412caf66fa1f45008394dfe64
Shivam Mistry <smistry1@vols.utk.edu>
2024-07-28 16:17:34 +0000

    completed project: social media blog api

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -18,6 +18,12 @@ public class MessageService {
         this.messageDao = messageDao;
     }
 
+    /**
+     * This method adds a message to the database given the account exists,
+     * and the message is valid -- not empty and below 256 characters.
+     * @param message contains information to add
+     * @return the newly added message if successful, otherwise null
+     */
     public Message addMessage(Message message) {
         AccountService accountService = new AccountService();
         Account account = accountService.getAccountById(message.getPosted_by());
@@ -29,14 +35,28 @@ public class MessageService {
         return null; 
     }
 
+    /**
+     * This method retrieves all messages from the database
+     * @return a list of messages
+     */
     public List<Message> getAllMessages() {
         return messageDao.getAllMessages();
     }
 
+    /**
+     * This method retrieves a message by id, if exists
+     * @param id the message_id
+     * @return Message if it exist, otherwise null
+     */
     public Message getMessageById(int id) {
         return messageDao.getMessageById(id);
     }
 
+    /**
+     * This method deletes a message using the message_id
+     * @param id the message_id
+     * @return the deleted Message object, if it exist, otherwise null
+     */
     public Message deleteMessageById(int id) {
         Message messageToDelete = getMessageById(id);
         if (messageToDelete != null) {
@@ -45,6 +65,13 @@ public class MessageService {
         return messageToDelete;
     }
 
+    /**
+     * This method updates the content of the message given that message exists,
+     * and the text is not empty and contains less than 256 characters.
+     * @param id message to update
+     * @param text new content to put in the message
+     * @return the updated Message if successful, otherwise null
+     */
     public Message updateMessageById(int id, String text) {
         if (getMessageById(id) != null &&
             !text.isEmpty() &&
@@ -55,6 +82,11 @@ public class MessageService {
         return null; 
     }
 
+    /**
+     * This method retrieves a list of all messages by a particular user
+     * @param userId contains the user id
+     * @return a list of all messages if successful, otherwise null
+     */
     public List<Message> getAllMessagesByUser(int userId) {
         return messageDao.getAllMessagesByUser(userId);
     }
